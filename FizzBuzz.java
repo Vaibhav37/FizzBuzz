e1f6674680449111077b0ab0236c50b5ecf1ce39
Vaibhav37 <ug201210037@iitj.ac.in>
2018-02-14 20:59:45 +0530

    Second Commit -> multiple mod removed

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -3,10 +3,12 @@ package FizzBuzzPackage;
 public class FizzBuzz {
 	public String fizzbuzz_function(int number)
 	{
-		if(number %3 ==0 && number %5==0)
-			return "FizzBuzz";
-		if(number%3==0)
-			return "Fizz";
+		if(number %3 ==0)
+		{
+			if(number %5==0)
+				return "FizzBuzz";
+			return "Fizz";	
+		}
 		if(number%5==0)
 			return "Buzz";
 		return Integer.toString(number);
